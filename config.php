cc380335dc6bb0fe502a5cfd0d9a0fd67ef701e7
funkake <im.nevu@gmail.com>
2014-10-13 09:56:52 +0200

    Index.php - updated added stylesheet

--- config.php
+++ config.php
@@ -4,6 +4,12 @@ $db_username = 'moderator';
 $db_password = 'ftyujnbv';
 $db_name = 'barista';
 $db_host = 'localhost';
+try {
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 $con = new mysqli($db_host, $db_username, $db_password,$db_name);
+}
+catch(PDOException $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,55 +1,18 @@
 <!DOCTYPE html>
 <html>
 
+    <link rel="stylesheet" type="text/css" href="indexStyle.css">
+
     <head>
     </head>
 
     <body>
 
-        <title>Index ds</title>
+    <?php
 
-            <?php
-            /**
-             * Created by IntelliJ IDEA.
-             * User: fun
-             * Date: 23.09.14
-             * Time: 09:29
-             */
+        include_once 'sessiontest.php'
 
-
-
-
-            $mysqli = new mysqli("localhost", "moderator", "ftyujnbv", "barista", 3306);
-            if ($mysqli->connect_errno) {
-                echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-            }
-
-            $mysqli->query("INSERT INTO barista.produkt(produkt_navn,kat_id)VALUES ('Phpkaffe' , 1)");
-
-            $tables = $mysqli->query("SHOW TABLES FROM barista");
-
-            while($mainTable = mysqli_fetch_array($tables))
-            {
-                $attributes = $mysqli->query("SELECT * FROM " . $mainTable[0]);
-
-                echo "<h6>" . $mainTable[0] . "</h6>";
-
-                echo mysqli_fetch_field($attribute);
-
-                while($attribute = mysqli_fetch_array($attributes))
-                {
-                    echo $attribute[0];
-
-                    echo "<br>";
-                }
-                echo "<br>";
-                echo "<br>";
-            }
-
-            $mysqli->query("DELETE FROM barista.produkt WHERE produkt_navn='Phpkaffe'");
-
-
-            ?>
+    ?>
 
     </body>
 
