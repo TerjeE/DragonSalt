2e02ac71ebb1fc1966e1a2719f37dd54d3b0b81e
TerjeE <Terje_Eik@hotmail.com>
2014-10-20 09:24:25 +0200

    duududud

--- handlekurv.php
+++ handlekurv.php
@@ -1,33 +1,36 @@
 <div class="shopping-cart">
 
     <?php
+    $current_url = base64_encode("http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    if (isset($_SESSION["produkt"])) {
+        foreach ($_SESSION["produkt"] as $produkt) {
+            echo "<b>";
+            echo $produkt->navn . "<br>";
+            echo "</b>";
+            if (is_array($produkt->tilbehor)) {
 
 
-    foreach($_SESSION["produkt"] as $produkt)
-    {
-        echo "<b>";
-       echo $produkt->navn . "<br>";
-        echo "</b>";
-        if(is_array($produkt->tilbehor))
-        {
+                echo htmlentities(" Tilbehør: ");
+                foreach ($produkt->tilbehor as $tilbehor_id) {
+                    //echo $tilbehor_id;
 
+                    $tilbehor_navn = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM tilbehor WHERE tilbehor_id ='$tilbehor_id'"))['tilbehor_navn'];
+                    echo $tilbehor_navn;
+                    echo "\t";
+                }
+                echo "<br>";
+            } else {
+                echo htmlentities("Uten tilbehør");
+                echo "<br>";
 
-            echo htmlentities(" Tilbehør: ");
-            foreach($produkt->tilbehor as $tilbehor_id)
-            {
-                //echo $tilbehor_id;
-
-                $tilbehor_navn = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM tilbehor WHERE tilbehor_id ='$tilbehor_id'"))['tilbehor_navn'];
-                echo $tilbehor_navn;
-                echo "\t";
             }
-            echo "<br>";
-        }else{
-            echo htmlentities("Uten tilbehør");
-            echo "<br>";
 
         }
-
+        //Knapper
+        echo "<br>";
+        echo '<span class="empty-cart"><a href="cart_update.php?emptycart=1&return_url='.$current_url.'">Empty Cart</a></span>';
+    }else{
+        echo "Empty cart";
     }
 
     ?>
