57bb52f729cf77e7811654e22cb084fce157803b
TerjeE <Terje_Eik@hotmail.com>
2014-10-24 15:08:58 +0200

    fikset litt på javascriptet. openBox

--- DS/JS/menyTest.js
+++ DS/JS/menyTest.js
@@ -53,11 +53,18 @@ function openBox(produktnavn)
 
     alle = document.getElementsByClassName("popup");
 
-    for(var en in alle){
+    /*for(var en in alle){
         if(en!=ting){
             en.style.display = "none";
         }
+    }*/
+
+    for(var i=0; i<alle.length; i++) {
+        if(alle[i] != ting) {
+            alle[i].style.display = "none";
+        }
     }
+
 
     if(ting.style.display == "none" || (ting.style.display != "none" && ting.style.display != "block")){
         ting.style.display = "block";
