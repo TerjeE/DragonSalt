0db272aba247a9971e372fbf00d93d9e7a5717cf
TerjeE <Terje_Eik@hotmail.com>
2014-11-03 10:35:04 +0100

    begynt på basic admin side

--- DS/PHP/admin.php
+++ DS/PHP/admin.php
@@ -7,6 +7,49 @@ if(isset($_SESSION['id']) & isset($_SESSION['username'])){
         <div>
             Hello mr admin!
         </div>
+
+        <div>
+            <form name="input" action="admin.php" method="post">
+                <table>
+                    <tr>
+                        <td>
+                            <label id="produkt_navn">Produkt navn:</label>
+                        </td>
+                        <td>
+                            <label id="kat_id">Kategori:</label>
+                        </td>
+                        <td>
+                            <label id="bilde">Bilde:</label>
+                        </td>
+                        <td>
+                            <label id="beskrivelse">Beskrivelse:</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input id="produkt_navn" type="text" name="produkt_navn" value="">
+                        </td>
+                        <td>
+                            <select name="kat_id">
+                                <option value="1">Kaffe</option>
+                                <option value="2">Te</option>
+                            </select>
+                        </td>
+                        <td>
+                            <input id="bilde" type="text" name="bilde" value="pics/">
+                        </td>
+                        <td>
+                            <input id="beskrivelse" type="text" name="beskrivelse" value="">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="submit" value="Submit">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     <?php
     }else{
         $error = 1;
