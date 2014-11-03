4f45ca93f0dd1eebc7011514f5e082456f07aa19
TerjeE <Terje_Eik@hotmail.com>
2014-11-03 14:23:20 +0100

    Ordner til at det blir lagt i ordre

--- DS/PHP/hk_empty.php
+++ DS/PHP/hk_empty.php
@@ -5,6 +5,7 @@ if (isset($_GET["emptycart"]) && $_GET["emptycart"] == 1) {
     $return_url = base64_decode($_GET["return_url"]); //return url
     //session_destroy();
     $_SESSION["produkt"] = array();
+    $_SESSION["ferdigprodukt"] = array();
     header('Location:' . $return_url);
 }
 ?>
---
+++ DS/PHP/hk_tilordre.php
@@ -0,0 +1,40 @@
+<?php
+include_once("classes.php");
+include_once("config.php");
+
+if (isset($_SESSION["ferdigprodukt"])) {
+
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < 20; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+
+    $navn = $randomString;
+    $dato = date("Y/m/d");
+    $sql = 'INSERT INTO `ordre`(`ordre_navn`, `dato`) VALUES ("'.$navn.'", "'.$dato.'")';
+
+    /*echo "<br>";
+    echo $sql;
+    echo "<br>";*/
+    if (!mysqli_query($con, $sql)) {
+        die('Error: ' . mysqli_error($con));
+    }
+    $ordre_id = mysqli_insert_id($con);
+
+    foreach ($_SESSION["ferdigprodukt"] as $ferdigprodukt) {
+
+        $sql = 'INSERT INTO `ferdig_ordre`(`fp_id`, `ordre_id`) VALUES ("'.$ferdigprodukt .'", '.$ordre_id .')';
+
+        /*echo "<br>";
+        echo $sql;
+        echo "<br>";*/
+        if (!mysqli_query($con, $sql)) {
+            die('Error: ' . mysqli_error($con));
+        }
+
+    }
+}
+
+
+?>
