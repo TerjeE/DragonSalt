350180ed7acbfe45d0cbf2653b4a14013232cae9
TerjeE <Terje_Eik@hotmail.com>
2014-11-09 22:44:57 +0100

    fikset bug i handlekurv

--- DS/PHP/handlekurv.php
+++ DS/PHP/handlekurv.php
@@ -38,7 +38,7 @@
             echo " kr";
             $totalpris += $produkt->pris;
             echo "</div>";
-            echo '<button class="fjern-produkt"><a href="PHP/hk_fjern.php?return_url=' . $current_url . '&Hk_index='.$index.'">X</a></button>';
+            echo '<button class="fjern-produkt"><a onclick="scrollTest(this)" href="PHP/hk_fjern.php?return_url=' . $current_url . '&Hk_index='.$index.'">X</a></button>';
             $index++;
             echo "</div>";
         }
@@ -54,9 +54,10 @@
 
         //Knapper
         echo "<br>";
-        echo '<button class="empty-cart"><a href="PHP/hk_empty.php?emptycart=1&return_url=' . $current_url . '">Empty Cart</a></button>';
+        echo '<button class="empty-cart"><a  onclick="scrollTest(this)" href="PHP/hk_empty.php?emptycart=1&return_url=' . $current_url . '">Empty Cart</a></button>';
+
         echo "<br>";
-        echo '<button class="checkBtn"><a href="PHP/hk_tilfp.php?return_url=' . $current_url . '">Test</a></button>';
+        echo '<button class="checkBtn"><a  onclick="scrollTest(this)" href="PHP/hk_tilfp.php?return_url=' . $current_url . '">Test</a></button>';
         echo "</div>";
     } else {
         echo "Empty cart";
