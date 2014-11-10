b9f2de4fea48cc16a66f8845438faa651c606518
TerjeE <Terje_Eik@hotmail.com>
2014-11-10 01:29:19 +0100

    kvittering styr

--- DS/PHP/hk_ordre.php
+++ DS/PHP/hk_ordre.php
@@ -9,6 +9,7 @@ if (session_status() == PHP_SESSION_NONE) {
 if (isset($_GET["ordre_navn"])) {
     //session_destroy();
     ?>
+<link rel="stylesheet" type="text/css" href="kvittering.css">
 <div class="kvittering_produktliste">
     Din ordre:
 
